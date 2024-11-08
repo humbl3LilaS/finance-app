e2003daf0ba28fbaa4f4e7a6b7bcc30b2aea4ad3
humbl3LilaS <sabishinekobebe@gmail.com>
2024-11-08 20:39:15 +0630

    fix: generalize the layout of route under (auth) route group

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,33 @@
+import {ClerkLoaded, ClerkLoading} from "@clerk/nextjs";
+import {Loader2} from "lucide-react";
+import Image from "next/image";
+
+const Layout = ({children}: Readonly<{ children: React.ReactNode; }>) => {
+    return (
+        <section className={"min-h-screen grid grid-cols-1 lg:grid-cols-2"}>
+
+            <div className={"h-full lg:flex flex-col items-center justify-center px-4 gap-y-8"}>
+                <header className={"text-center space-y-4 pt-16`"}>
+                    <h1 className={"font-bold text-3xl text-[#2E2A47]"}>Welcome Back!</h1>
+                    <p className={"text-base text-[#7E8CA0]"}>
+                        Log in or Create account to get back to your dashboard!
+                    </p>
+                </header>
+                <div className={"flex items-center justify-center"}>
+                    <ClerkLoaded>
+                        {children}
+                    </ClerkLoaded>
+                    <ClerkLoading>
+                        <Loader2 className={"mt-8 animate-spin text-muted-foreground size-10"}/>
+                    </ClerkLoading>
+                </div>
+            </div>
+
+            <div className={"hidden h-full bg-blue-700 lg:flex items-center justify-center"}>
+                <Image src={"/logo.svg"} alt={"logo"} width={80} height={80}/>
+            </div>
+        </section>
+    );
+};
+
+export default Layout;
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,31 +1,8 @@
-import {ClerkLoaded, ClerkLoading, SignUp} from "@clerk/nextjs";
-import {Loader2} from "lucide-react";
-import Image from "next/image";
+import {SignUp} from "@clerk/nextjs";
 
 const SignUpPage = () => {
     return (
-        <section className={"min-h-screen grid  grid-cols-1 lg:grid-cols-2"}>
-            <div className={"h-full py-8 lg:flex flex-col items-center justify-center px-4 gap-y-8"}>
-                <header className={"text-center space-y-4 pt-16`"}>
-                    <h1 className={"font-bold text-3xl text-[#2E2A47]"}>Welcome Back!</h1>
-                    <p className={"text-base text-[#7E8CA0]"}>
-                        Log in or Create account to get back to your dashboard!
-                    </p>
-                </header>
-                <div className={"flex items-center justify-center"}>
-                    <ClerkLoaded>
-                        <SignUp path={"/sign-up"} />
-                    </ClerkLoaded>
-                    <ClerkLoading>
-                        <Loader2 className={"mt-8 animate-spin text-muted-foreground size-10"}/>
-                    </ClerkLoading>
-                </div>
-            </div>
-
-            <div className={"hidden h-full bg-blue-700 lg:flex items-center justify-center"}>
-                <Image src={"/logo.svg"} alt={"logo"} width={80} height={80}/>
-            </div>
-        </section>
+        <SignUp path={"/sign-up"}/>
     );
 };
 
